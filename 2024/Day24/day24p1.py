e4b3d7b6305fca0e2e09da0f4ac4a4b499326e2c
sharjeelbhullar <sharjeelbhullar@gmail.com>
2024-12-24 14:22:11 +0500

    Add Day 24 files

---
+++ 2024/Day24/day24p1.py
@@ -0,0 +1,40 @@
+import os
+
+input_path = os.path.join(os.path.dirname(__file__), 'input.txt')
+file = open(input_path)
+
+known = {}
+
+for line in file:
+    if line.isspace(): break
+    x, y = line.split(": ")
+    known[x] = int(y)
+
+formulas = {}
+
+for line in file:
+    x, op, y, z = line.replace(" -> ", " ").split()
+    formulas[z] = (op, x, y)
+
+operators = {
+    "OR": lambda x, y: x | y,
+    "AND": lambda x, y: x & y,
+    "XOR": lambda x, y: x ^ y,
+}
+
+def calc(wire):
+    if wire in known: return known[wire]
+    op, x, y = formulas[wire]
+    known[wire] = operators[op](calc(x), calc(y))
+    return known[wire]
+
+z = []
+i = 0
+
+while True:
+    key = "z" + str(i).rjust(2, "0")
+    if key not in formulas: break
+    z.append(calc(key))
+    i += 1
+
+print(int("".join(map(str, z[::-1])), 2))
