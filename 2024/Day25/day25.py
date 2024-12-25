e767b329892a4be23ccf0ee2775d5a4bf2372965
sharjeelbhullar <sharjeelbhullar@gmail.com>
2024-12-25 11:02:43 +0500

    Add Day 25 files

---
+++ 2024/Day25/day25.py
@@ -0,0 +1,19 @@
+import os
+locks = []
+keys = []
+input_path = os.path.join(os.path.dirname(__file__), 'input.txt')
+for block in open(input_path).read().split("\n\n"):
+    grid = list(zip(*block.splitlines()))
+    if grid[0][0] == "#":
+        locks.append([row.count("#") - 1 for row in grid])
+    else:
+        keys.append([row.count("#") - 1 for row in grid])
+
+total = 0
+
+for lock in locks:
+    for key in keys:
+        if all(x + y <= 5 for x, y in zip(lock, key)):
+            total += 1
+
+print(total)
